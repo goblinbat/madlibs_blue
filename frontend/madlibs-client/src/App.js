ff9968fc3660f38358bbc26829ca4411d703fad3
Molly Hopper <mollyhopper95@gmail.com>
2019-07-11 16:52:06 -0400

    setting up front end skeleton

---
+++ frontend/madlibs-client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const [sessionToken, setSessionToken] = useState(undefined);
+  
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
